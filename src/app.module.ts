4a425ccaf33dc441536636be75fd66ca2ecbceed
ToseebNadaf <toseebnadaf@gmail.com>
2025-02-21 01:37:30 +0530

    Created required files & added jwt auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PrismaService } from './prisma.service';
+import { CompanyModule } from './company/company.module';
+import { JobModule } from './job/job.module';
+import { ApplicationModule } from './application/application.module';
+import { ApplicationService } from './application/application.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('SECRET_KEY'),
+        signOptions: { expiresIn: '1d' },
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule,
+    UserModule,
+    CompanyModule,
+    JobModule,
+    ApplicationModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService, PrismaService, ApplicationService],
 })
 export class AppModule {}
