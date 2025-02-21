e5a4014a202fb406ae1f876068bb0c18e8ac30d1
ToseebNadaf <toseebnadaf@gmail.com>
2025-02-22 00:29:54 +0530

    Complete route and controller for user section

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Put,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
+import { Response } from 'express';
 import { UserService } from './user.service';
-import { RegisterUserDto } from './dto/user.dto';
+import { RegisterUserDto, UpdateUserDto } from './dto/user.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('user')
 export class UserController {
@@ -9,5 +20,71 @@ export class UserController {
   @Post('register')
   async register(@Body() registerUserDto: RegisterUserDto) {
     return this.userService.register(registerUserDto);
+  }
+
+  @Post('login')
+  async login(@Body() body, @Res() res: Response): Promise<Response> {
+    const { email, password, role } = body;
+
+    try {
+      const userResponse = await this.userService.login(email, password, role);
+
+      res.cookie('token', userResponse?.token, {
+        maxAge: 24 * 60 * 60 * 1000, // 1 day in milliseconds
+        httpOnly: true,
+        sameSite: 'strict',
+      });
+
+      return res.status(200).json(userResponse);
+    } catch (error) {
+      return res.status(error?.status || 500).json({
+        message: error?.message || 'Internal server error',
+        success: false,
+      });
+    }
+  }
+
+  @Get('logout')
+  async logout(@Res() res: Response) {
+    try {
+      const result = await this.userService.logout();
+
+      res.cookie('token', '', {
+        maxAge: 0,
+        httpOnly: true,
+        sameSite: 'strict',
+      });
+
+      return res.status(200).json(result);
+    } catch (error) {
+      return res.status(500).json({
+        message: 'Internal server error',
+        success: false,
+      });
+    }
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Put('updateProfile')
+  async updateProfile(
+    @Req() req: any,
+    @Body() updateUserDto: UpdateUserDto,
+    @Res() res: Response,
+  ) {
+    try {
+      const userId = req.user.id;
+      const user = await this.userService.updateProfile(userId, updateUserDto);
+
+      return res.status(200).json({
+        message: 'Profile updated successfully',
+        user,
+        success: true,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        message: 'Internal server error',
+        success: false,
+      });
+    }
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,7 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma.service';
-import { RegisterUserDto } from './dto/user.dto';
+import { RegisterUserDto, UpdateUserDto } from './dto/user.dto';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
@@ -61,5 +61,72 @@ export class UserService {
     }
 
     return { user, success: true, message: 'User Created Successfully' };
+  }
+
+  async login(email: string, password: string, role: string) {
+    if (!email || !password || !role) {
+      throw new BadRequestException('All fields are required');
+    }
+
+    const user = await this.prisma.user.findUnique({ where: { email } });
+    if (!user) {
+      throw new BadRequestException('User not exist');
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      throw new BadRequestException('Incorrect password');
+    }
+
+    if (role !== user.role) {
+      throw new BadRequestException("Account dosen't exist with current role");
+    }
+
+    const token = this.jwtService.sign(
+      { userId: user.id },
+      { secret: process.env.SECRET_KEY, expiresIn: '1d' },
+    );
+
+    return {
+      token,
+      user,
+      success: true,
+      message: 'User Logged In Successfully',
+    };
+  }
+
+  async logout(): Promise<{ message: string; success: boolean }> {
+    return { message: 'Logged Out successfully', success: true };
+  }
+
+  async updateProfile(id: string, updateUserDto: UpdateUserDto) {
+    const {
+      fullname,
+      email,
+      phoneNumber,
+      profileBio,
+      profileSkills,
+      profileResume,
+      profilePhoto,
+    } = updateUserDto;
+
+    if (!fullname || !email || !phoneNumber || !profileBio || !profileSkills) {
+      throw new BadRequestException('All fields are required');
+    }
+
+    const user = await this.prisma.user.update({
+      where: { id },
+      data: {
+        fullname,
+        email,
+        phoneNumber,
+        profileBio,
+        profileSkills,
+        profileResume,
+        profilePhoto,
+      },
+    });
+
+    return user;
   }
 }
