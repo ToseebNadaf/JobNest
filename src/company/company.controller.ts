48104b1afae32167e022ec46dce3325ba4824c21
ToseebNadaf <toseebnadaf@gmail.com>
2025-02-23 22:52:40 +0530

    Created company routes & controllers

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -1,4 +1,90 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { CompanyService } from './company.service';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { RegisterCompanyDto, UpdateCompanyDto } from './dto/company.dto';
 
 @Controller('company')
-export class CompanyController {}
+export class CompanyController {
+  constructor(private readonly companyService: CompanyService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Post('register')
+  async registerCompany(
+    @Req() req,
+    @Body() registerCompanyDto: RegisterCompanyDto,
+  ) {
+    const userId = req.user.id;
+    const result = await this.companyService.registerCompany(
+      userId,
+      registerCompanyDto,
+    );
+    console.log(result, 'result');
+
+    return {
+      message: 'Company created successfully',
+      result,
+      success: true,
+    };
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  async getCompanies(@Req() req: any) {
+    const userId = req.user.id;
+    console.log(userId, 'userId');
+
+    const result = await this.companyService.getCompanies(userId);
+
+    return {
+      result,
+      success: true,
+    };
+  }
+
+  @Get(':id')
+  async getCompanyByid(@Param('id') companyId: string) {
+    const company = await this.companyService.getCompanyById(companyId);
+    return {
+      company,
+      success: true,
+    };
+  }
+
+  @Delete(':id')
+  async deleteCompany(@Param('id') companyId: string) {
+    const result = await this.companyService.deleteCompany(companyId);
+    return {
+      result,
+      success: true,
+      message: 'Company deleted successfully',
+    };
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Put(':id')
+  async updateCompany(
+    @Param('id') companyId: string,
+    @Body() updateCompanyDto: UpdateCompanyDto,
+  ) {
+    const result = await this.companyService.updateCompany(
+      companyId,
+      updateCompanyDto,
+    );
+
+    return {
+      result,
+      success: true,
+      message: 'Company updated successfully',
+    };
+  }
+}
--- src/company/company.service.ts
+++ src/company/company.service.ts
@@ -1,4 +1,89 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { RegisterCompanyDto, UpdateCompanyDto } from './dto/company.dto';
 
 @Injectable()
-export class CompanyService {}
+export class CompanyService {
+  constructor(private prisma: PrismaService) {}
+
+  async registerCompany(
+    userId: string,
+    registerCompanyDto: RegisterCompanyDto,
+  ) {
+    const { name, description, website, location, logo } = registerCompanyDto;
+
+    const existingCompany = await this.prisma.company.findUnique({
+      where: { name },
+    });
+
+    if (existingCompany) {
+      throw new BadRequestException("You can't add same company.");
+    }
+
+    const company = await this.prisma.company.create({
+      data: {
+        name,
+        description,
+        website,
+        location,
+        logo,
+        userId,
+      },
+    });
+
+    return company;
+  }
+
+  async getCompanies(userId: string) {
+    const companies = await this.prisma.company.findMany({
+      where: { userId },
+    });
+
+    if (!companies || companies?.length == 0) {
+      throw new NotFoundException('Company not found');
+    }
+
+    return companies;
+  }
+
+  async getCompanyById(id: string) {
+    const company = await this.prisma.company.findUnique({
+      where: { id },
+    });
+
+    if (!company) {
+      throw new NotFoundException('Company not found');
+    }
+
+    return company;
+  }
+
+  async deleteCompany(id: string) {
+    const company = await this.prisma.company.delete({
+      where: { id },
+    });
+
+    if (!company) {
+      throw new NotFoundException('Company not deleted');
+    }
+
+    return company;
+  }
+
+  async updateCompany(id: string, updateCompanyDto: UpdateCompanyDto) {
+    const company = await this.prisma.company.update({
+      where: { id },
+      data: updateCompanyDto,
+    });
+
+    if (!company) {
+      throw new NotFoundException('Company not found');
+    }
+
+    return company;
+  }
+}
